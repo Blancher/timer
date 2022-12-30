d0ae14190c0c99a6afaf0e54407faae94f716a36
Blancher <108644584+Blancher@users.noreply.github.com>
2022-12-30 10:45:36 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+// Variables
+
+const startStopBtn = document.querySelector('#startStopBtn');
+const resetBtn = document.querySelector('#resetBtn');
+const submitBtn = document.querySelector('#submit');
+const displayTimer = document.querySelector('#timer');
+const audio = document.querySelector('audio');
+const domSeconds = document.querySelector('#seconds');
+const domMinutes = document.querySelector('#minutes');
+const domHours = document.querySelector('#hours');
+const endMessage = document.querySelector('#endmessage');
+
+// Times
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let interval;
+
+// Functions
+
+function valueAssigner(value, num) {
+    if (parseInt(value) > 0) {
+        return parseInt(value);
+    } else {
+        return num;
+    }
+}
+function addZeroes(time) {
+    if ((`${time}0`).length > 2) {
+        return time;
+    } else {
+        return `0${time}`;
+    }
+}
+
+function stopWatch() {
+    if (hours >= 0) {
+        seconds--;
+        if (seconds < 0) {
+            seconds = 59;
+            minutes--;
+        }
+        if (minutes < 0) {
+            minutes = 59;
+            hours--;
+        }
+        seconds = addZeroes(seconds);
+        minutes = addZeroes(minutes);
+        hours = addZeroes(hours);
+        if (hours === -1) {
+            displayTimer.textContent = '00:00:00';
+        } else {
+            displayTimer.textContent = `${hours}:${minutes}:${seconds}`;
+        }
+    } else {
+        startStopBtn.textContent = 'Start';
+        endMessage.textContent = 'Your timer is over.';
+        document.querySelector('audio').play();
+    }
+}
+
+// Event listeners
+
+startStopBtn.addEventListener('click', () => {
+    seconds = valueAssigner(domSeconds.value, seconds);
+    minutes = valueAssigner(domMinutes.value, minutes);
+    hours = valueAssigner(domHours.value, hours);
+    displayTimer.textContent = `${addZeroes(hours)}:${addZeroes(minutes)}:${addZeroes(seconds)}`;
+    endMessage.textContent = '';
+    if (startStopBtn.textContent === 'Start') {
+        interval = setInterval(stopWatch, 1000);
+        startStopBtn.textContent = 'Pause';
+    } else if (startStopBtn.textContent === 'Pause') {
+        clearInterval(interval);
+        startStopBtn.textContent = 'Start';
+    }
+});
+resetBtn.addEventListener('click', () => {
+    clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    domSeconds.value = "";
+    domMinutes.value = "";
+    domHours.value = "";
+    displayTimer.textContent = '00:00:00';
+    startStopBtn.textContent = 'Start';
+});
